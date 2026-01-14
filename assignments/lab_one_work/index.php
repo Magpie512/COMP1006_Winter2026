bf42ef66927889dc2bc416534720df1f3ae975ab
Magpie512 <224426406+Magpie512@users.noreply.github.com>
2026-01-14 18:23:32 -0500

    Refactor index.php to remove redundant require_once and include statements for car.php

--- assignments/lab_one_work/index.php
+++ assignments/lab_one_work/index.php
@@ -1,8 +1,6 @@
 <?php
-
-require_once "car.php";
-include "car.php";
 declare(strict_types=1); // Enable strict types
+require "car.php";
 $car = new Car("Toyota", "Camry", 2020);
 echo $car->displayInfo();
 
