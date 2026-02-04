f7d939545652a75ec350bc35cfb642830f9f7007
Magpie512 <224426406+Magpie512@users.noreply.github.com>
2026-02-04 14:56:51 -0500

    a

---
+++ TrollPost/includes/footer.php
@@ -0,0 +1,7 @@
+<footer>
+    <p> &copy; 2026 Troll Post </p>
+    <p> Created by <abbr title="The Evokers Department of The Aethermancy Guild">TEDTAG</abbr></p>
+    <p><small> Really made by Mara Briggs</small></p>
+</footer>
+</body>
+</html>
