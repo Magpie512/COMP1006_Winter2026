217322712446432682d18c6bbecf4ed31c31b0d7
Magpie512 <224426406+Magpie512@users.noreply.github.com>
2026-02-05 16:58:27 -0500

    a

--- TrollPost/includes/header.php
+++ TrollPost/includes/header.php
@@ -11,14 +11,14 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
     <link rel="icon" type="image/png" href="img/gob.png">
-    <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="styles/style.css?v=1.3"> <!-- Cache-busting query parameter -->
 </head>
 
 <body>
-<header>
+<header class="header">
     <img src="img/gob.png" class="logo">
     <h1 class="embossed"> TrollPost </h1>
+    <input type="text" id="searchBar" placeholder="Scry?" class="form-control">
     <button id="signin" class="btn btn-primary"> Sign In </button>
     <button id="signup" class="btn btn-secondary"> Sign Up </button>
-    <input type="text" id="searchBar" placeholder="Scry?" class="form-control">
 </header>
