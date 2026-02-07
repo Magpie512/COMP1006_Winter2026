a602bf87bb7608e7aedd90217f7bfc5e86d5caff
Magpie512 <224426406+Magpie512@users.noreply.github.com>
2026-02-07 14:05:26 -0500

    Update footer styles and enhance layout for HOTWContainer

--- TrollPost/includes/footer.php
+++ TrollPost/includes/footer.php
@@ -1,7 +1,7 @@
 <footer>
     <p> &copy; 2026 Troll Post </p>
     <p> Created by <abbr title="The Evokers Department of The Aethermancy Guild">TEDTAG</abbr></p>
-    <p><small> Really made by Mara Briggs</small></p>
+    <p>Really made by Mara Briggs</p>
 </footer>
 </body>
 </html>
